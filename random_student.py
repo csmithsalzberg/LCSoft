198389b73c22b55839e40948cdcf83b25e77aee8
TakingTheL <yliu6@stuy.edu>
2017-09-13 11:49:31 -0400

    added random student selection

---
+++ random_student.py
@@ -0,0 +1,31 @@
+import random
+
+CLASSES = {
+        7: [ 'Helen', 'Shakil', 'Eric', 'Jennifer Y', 'Jennifer Z', 'Arif', 'Queenie', 'Jawadul', 'Shaina', 'Vivien', 'Brian', 'Naotaka', 'Bayan', 'Adam', 'Caleb', 'Terry', 'Jason', 'Alessandro', 'Samantha', 'Carol', 'Joyce', 'Shannon', 'Charles', 'Taylor', 'Kelly', 'Leo', 'Khyber', 'Ibnul', 'Eugene', 'Yuyang', 'Karina', 'Tiffany', 'Holden', 'Michael'],
+        8: ['Masha', 'Adrian', 'David', 'Eric', 'Josh', 'Jerome', 'Henry', 'Jackie', 'Giorgio', 'Karen', 'Sonal', 'Xavier', 'Bermet', 'Alex', 'Iris', 'Manahal', 'Donia', 'Md', 'Connie', 'Lisa', 'Xing', 'Angelica', 'Angel', 'Augie', 'Dimitriy', 'Yiduo', 'Gordon', 'Tiffany', 'Clive', 'Jonathan', 'Sasha', 'Daniel'],
+        9: [ 'Yu Qi', 'Michela', 'Kristin', 'Fabiha', 'Maxim', 'Marcus', 'Ish', 'James', 'Ryan', 'Edward', 'Adeeb', 'Jake', 'Cynthia', 'Kevin', 'Levi', 'Edmond', 'Kyle', 'Andrew', 'Max', 'Jenny', 'Philip', 'Shan', 'Mansour', 'Ray', 'Jake', 'Ida', 'Kerry', 'Stanley', 'Jackie', 'William', 'Tina', 'Michael']
+}
+
+def random_student(period):
+    """
+    Prints the name of a random student from a specified period.
+
+    Arg:
+    int period class period to choose from
+
+    Ret:
+    str name of a student from the specified period. 
+    """
+    try:
+        class_list = CLASSES[period]
+        random_student_num = random.randint(0, len(class_list) - 1)
+        return class_list[random_student_num]
+    except:
+        return 'not a valid period'
+
+
+
+print random_student(4)
+print random_student(8)
+print random_student(9)
+print random_student(11)
